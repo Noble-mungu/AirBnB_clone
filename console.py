e12cea44f997ba9bb814a0b36b074973a2e5fbbd
Noble Mungu <mungunoble@gmail.com>
2022-08-09 22:03:56 +0300

    updatesss

--- console.py
+++ console.py
@@ -1,185 +1,162 @@
 #!/usr/bin/python3
 """
-Defines the HBNBCommand class
+    Console module
 """
 
 import cmd
-from signal import signal, SIGINT
-from sys import exit
 from models.base_model import BaseModel
-import models
-import shlex
 from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-
+import json
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Entry point of the command interpreter
-    """
-    prompt = "(hbnb)"
-    allowed_classes = [
-        "BaseModel", "User", "State", "City", "Amenity", "Place", "Review"
-        ]
+    """ Console prompt  """
+    prompt = "(hbnb) "
 
-    def do_quit(self, *args):
-        """Quit command to exit the program."""
-        return True
+    """ --Classes """
+    __classes = ['BaseModel', 'User']
 
-    def do_EOF(self, *args):
-        """Exit the program"""
-        return True
+
+    """ --Commands """
+    __commands = ['create', 'show', 'destroy', 'all', 'update']
+
 
     def emptyline(self):
-        """Empty line"""
+        """do nothing when empty"""
         pass
 
-    def handler(signal_received, frame):
-        """Handle the SIGINT or CTRL-C signal"""
-        print("^C")
-        exit(0)
+    def do_quit(self, arg):
+        """exit program when arg is quit"""
+        raise SystemExit
 
-    def do_create(self, args):
+    def do_EOF(self, arg):
+        """exits program when arg is EOF"""
+        raise SystemExit
+    
+    def do_create(self, prmArg):
         """
-        Create a new instance of a class
+        creates a new instance of BaseModel, 
+        saves it to the JSON file and prints the id.
         """
-        # Check if a class name is supplied as the argument.
-        if len(args) == 0:
-            self.err_handler(1)
+        if not prmArg:
+            raise ValueError("** class name missing **")
+        elif prmArg not in self.__classes:
+            raise ValueError("** class doesn't exist **")
         else:
-            args = args.split()
-            if args[0] in self.allowed_classes:
-                # Create a new instance of the class
-                new_instance = eval(args[0])(args[1:])
-                print(new_instance.id)
-                new_instance.save()
-            else:
-                self.err_handler(2)
+            model_classes = {'BaseModel': BaseModel, 'User': User}
+            my_model = model_classes[prmArg]()
+            print(my_model.id)
+            storage.save()    
 
-    def do_show(self, args):
-        """
-        Prints the string representation of an instance based on the class name
-        and id
-        """
-        if len(args) == 0:
-            self.err_handler(1)
+    def do_show(self, prmArg):
+        """ prints the string representation of an instance based on the class name and id """
+
+        if not prmArg:
+            raise ValueError(" ** class name missing **")
+            return
+
+        prmArgs = prmArg.split(' ')
+
+        if prmArgs[0] not in self.__classses:
+            raise ValueError("** class doesn't exist **")
+        elif len(prmArgs) == 1:
+            raise ValueError("** instance id missing **")
+        else:
+            dict_obj = storage.all()
+            key = "{}.{}".format(prmArgs[0], prmArgs[1])
+            if key not in dict_obj:
+                raise ValueError("** no instance found **")
+
+            print(dict_obj[key])
+
+    def do_destroy(self, prmArgs):
+        """ deletes an instance based on the class name ad id """
+        if not prmArgs:
+            raise ValueError("** class name missing **")
+
+        args = prmArgs.split(' ')
+        
+        if args[0] not in self.__classes:
+            raise ValueError("** class doesn't exist **")
         elif len(args) == 1:
-            self.err_handler(3)
+            raise ValueError("** instance id missing **")
         else:
-            args = args.split()
-            if args[0] in self.allowed_classes:
-                models.storage.reload()
-                # Create an identifier based on user input.
-                identifier = args[0] + "." + args[1]
-                if identifier in list(models.storage.all().keys()):
-                    print(models.storage.all()[identifier])
-                else:
-                    self.err_handler(4)
-            else:
-                self.err_handler(2)
+            dict_obj = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            if key not in dict_obj:
+                raise ValueError("** no instance found **")
 
-    def do_destroy(self, args):
-        """
-        Deletes an instance based on the class name and id
-        """
-        args = args.split()
-        if len(args) == 0:
-            self.err_handler(1)
-        elif len(args) == 1:
-            if args[0] in self.allowed_classes:
-                self.err_handler(3)
-            else:
-                self.err_handler(2)
-        else:
-            if args[0] in self.allowed_classes:
-                models.storage.reload()
-                identifier = args[0] + "." + args[1]
-                if identifier in list(models.storage.all().keys()):
-                    del models.storage.all()[identifier]
-                    models.storage.save()
-                else:
-                    self.err_handler(4)
-            else:
-                self.err_handler(2)
+            del dict_obj[key]
+            storage.save()
 
-    def do_all(self, args):
-        """
-        Prints all string representation of all instances
-        based or not on the class name.
-        """
-        str_list = []
-        # If length is zero, print all instances.
-        if len(args) == 0:
-            all_dict = models.storage.all()
-            for id in all_dict.keys():
-                obj = all_dict[id]
-                str_list.append(str(obj))
-            print(str_list)
-        else:
-            models.storage.reload()
-            args = args.split()
-            if args[0] in self.allowed_classes:
-                all_dict = models.storage.all()
-                for id in all_dict.keys():
-                    if args[0] in id:
-                        obj = all_dict[id]
-                        str_list.append(str(obj))
-                print(str_list)
-            else:
-                self.err_handler(2)
+    def do_all(self, prmArgs):
+        """ prints all string representarion of all instances based or not on the class name """
+        args = prmArgs.split(' ')
+        
+        if args and args[0] not in self.__classes:
+            raise ValueError("** class doesn't exist **")
+        
+        dict = storage.all()
+        list = []
 
-    def do_update(self, args):
-        """
-        Updates an instance based on the class name and id
-        by adding or updating attribute
-        """
-        args = shlex.split(args)
-        if len(args) == 0:
-            self.err_handler(1)
-        elif len(args) == 1:
-            if args[0] in self.allowed_classes:
-                self.err_handler(3)
-            else:
-                self.err_handler(2)
-        else:
-            if args[0] in self.allowed_classes:
-                models.storage.reload()
-                identifier = args[0] + "." + args[1]
-                if identifier in list(models.storage.all().keys()):
-                    if len(args) == 2:
-                        self.err_handler(5)
-                    elif len(args) == 3:
-                        self.err_handler(6)
-                    else:
-                        attr = args[2]
-                        value = args[3]
-                        setattr(models.storage.all()[identifier], attr, value)
-                        models.storage.all()[identifier].save()
-                else:
-                    self.err_handler(4)
-            else:
-                self.err_handler(2)
+        for key, value in dict.items():
+            obj_name = value.__class__.__name__
+            if(not args or args[0] or obj_name == args[0]):
+                list += [value.__str__()]
 
-    def err_handler(self, error_num):
-        """Handles errors in the progrm"""
-        if error_num == 1:
-            print("** class name missing **")
-        elif error_num == 2:
-            print("** class doesn't exist **")
-        elif error_num == 3:
-            print("** instance id missing **")
-        elif error_num == 4:
-            print("** no instance found **")
-        elif error_num == 5:
-            print("** attribute name missing **")
-        elif error_num == 6:
-            print("** value missing **")
+        print(list)
 
+    def do_update(self, prmArg):
+        """ updates an instance based on the class name and id by adding or updating attribute(save the change into JSONfile) """
 
-if __name__ == "__main__":
-    signal(SIGINT, HBNBCommand.handler)
+        if not prmArg:
+            raise ValueError("** class name missing **")
+
+        args = prmArg.split(' ')
+
+        className, command, attribute, value = args
+
+        if args[0] not in self.__classes:
+            raise ValueError("** class doesn't exist **")
+        if len(args) == 1:
+            raise ValueError("** instance id missing **")
+
+        dict_obj = storage.all()
+        key = "{}.{}".format(args[0], args[1])
+        if key not in dict_obj:
+            raise ValueError("** no instance found **")
+        obj_value = dict_obj[key]
+
+        if len(args) == 2:
+            raise ValueError("** attribute name is missing **")
+
+        if len(args) == 3:
+            raise ValueError("** value is missing **")
+
+        if attribute not in ("id", "create_at", "updated_at"):
+            setattr(obj_value, attribute, self.__type(value))
+
+            storage.save()
+
+    def help_quit(self):
+        print("Quit command to exit the program\n")
+
+    def help_EOF(self):
+        print("EOF(end of file) command to exit the program\n")
+    
+    def help_create(self):
+        print("Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id.\n")
+
+    def help_show(self):
+        print("Prints the string representation of an instance based on the classname and id.\n")  
+
+    def help_destroy(self):
+        print("Deletes an instance based on the class name and id(save the change into the JSON file).\n")
+
+    def help_all(self):
+        print("Prints all the string representation of all instances based or not on the class name,\n")
+
+    def help_update(self):
+        print("Updates an instance based on the class name and id by \ adding or updating attribute (save the change into the JSON file).\n")
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
